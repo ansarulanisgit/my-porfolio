fe1c106f8ea82f5e7043c5ed49935d58d59e47a6
Ansarul Anis <ansarul.contact@gmail.comm>
2022-06-04 10:18:02 +0600

    Created and added components.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Experience from '../Experience/Experience';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Nav from '../Nav/Nav';
+import Portfolio from '../Portfolio/Portfolio';
+import Services from '../Services/Services';
+import Testimonials from '../Testimonials/Testimonials';
 import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <Header />
+            <Nav />
+            <About />
+            <Experience />
+            <Services />
+            <Portfolio />
+            <Testimonials />
+            <Contact />
+            <Footer />
         </div>
     );
 };
